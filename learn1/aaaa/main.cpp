e1bf823b8c18c3f73bf1a5cc3e7952a94801c04a
lenovo_ming <eiyaming@163.com>
2019-09-30 11:52:08 +0800

    add all

---
+++ learn1/aaaa/main.cpp
@@ -0,0 +1,31 @@
+
+//#include "iostream"
+
+//using namespace std;
+
+//int main()
+//{
+ //   cout<<100<<endl;
+
+ //   return 0;
+//}
+//test.cpp源文件
+#include<opencv2/opencv.hpp>
+#include<iostream>
+ 
+using namespace std;
+using namespace cv;
+ 
+int main(int argc,char **argv)
+{
+	VideoCapture capture(0);
+	namedWindow("hhh",CV_WINDOW_AUTOSIZE);
+	while(true)
+	{	
+		Mat frame;
+		capture>>frame;
+		imshow("hhh",frame);
+		waitKey(30);
+	}
+	return 0;
+}
